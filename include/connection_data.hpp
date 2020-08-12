2e97485a77a8f1d32dd75db67434f574dccb93a5
Sigalga <sigalgalker@gmail.com>
2020-08-12 16:08:22 +0300

    proof of concept

--- include/connection_data.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_ASHS_CONNECTION_DATA_HPP
-#define INCLUDE_ASHS_CONNECTION_DATA_HPP
-
-#include "../src/connection_data/connection_data.hpp"
-
-#endif // INCLUDE_ASHS_CONNECTION_DATA_HPP
