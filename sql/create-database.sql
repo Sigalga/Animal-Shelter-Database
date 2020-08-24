22a69881ac79486af97ab70e1a5bbda973a029dc
Sigalga <sigalgalker@gmail.com>
2020-08-24 04:25:19 +0300

    made th pk column auto-incremental

--- sql/create-database.sql
+++ sql/create-database.sql
@@ -3,12 +3,14 @@ CREATE DATABASE `pet_book`;
 USE `pet_book`;
 
 CREATE TABLE `pets` (
-  `pet_id` int(11) NOT NULL,
+  `pet_id` int(11) NOT NULL AUTO_INCREMENT,
   `adopter_id` int(11),
   `name` varchar(50) NOT NULL,
   `age_months` int NOT NULL,
   `status` varchar(50) NOT NULL,
   PRIMARY KEY (`pet_id`)
 );
+
+ALTER TABLE `pets` AUTO_INCREMENT=1;
 
 INSERT INTO `pets` VALUES (1,NULL,'Dummy',0,'in shelter');
